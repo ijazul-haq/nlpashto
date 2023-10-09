ff9dae9eae65a5e6f8013837726c41e4e53eb61d
Ijazul Haq <ijazse@hotmail.com>
2023-10-09 21:57:06 +0800

    0.0.19

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from nlpashto.word_segmentation import Segmenter
 from nlpashto.pos_tagging import POSTagger
 from nlpashto.sentiment_analysis import POLD
 from nlpashto.utils import Cleaner
+from nlpashto import pashto
 
 def run(): pass
     
--- nlpashto/__init__.py
+++ nlpashto/__init__.py
@@ -3,3 +3,4 @@ from .word_segmentation import Segmenter
 from .pos_tagging import POSTagger
 from .sentiment_analysis import POLD
 from .utils import Cleaner
+from . import pashto
