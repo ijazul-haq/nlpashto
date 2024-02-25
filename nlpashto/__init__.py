96b4d6438478819d636feaf2ac1411b18ebe6516
Ijazul Haq <ijazse@hotmail.com>
2024-02-25 13:44:30 +0800

    removed

--- nlpashto/__init__.py
+++
@@ -1,6 +0,0 @@
-from .tokenization import Tokenizer
-from .word_segmentation import Segmenter
-from .pos_tagging import POSTagger
-from .sentiment_analysis import POLD
-from .utils import Cleaner
-from . import pashto
